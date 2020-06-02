30eeb079f23b0b38ac552ebadc373fe9d109b5f0
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-06-02 00:27:40 -0700

    Updated App.js, Register.js, and Signin.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
@@ -9,10 +8,6 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
-
-const app = new Clarifai.App({
-	apiKey: '6f7294a68a17400da26508e85a2b360b'
-});
 
 const particlesOptions = {
 	particles: {
@@ -26,23 +21,25 @@ const particlesOptions = {
 	}
 };
 
+const initialState = {
+	input: '',
+	imageUrl: '',
+	box: {},
+	route: 'signin',
+	isSignedIn: false,
+	user: {
+		id: '',
+		name: '',
+		email: '',
+		entries: 0,
+		joined: ''
+	}
+};
+
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			input: '',
-			imageUrl: '',
-			box: {},
-			route: 'signin',
-			isSignedIn: false,
-			user: {
-				id: '',
-				name: '',
-				email: '',
-				entries: 0,
-				joined: ''
-			}
-		};
+		this.state = initialState;
 	}
 
 	loadUser = (data) => {
@@ -80,30 +77,39 @@ class App extends Component {
 
 	onImageSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then((response) => {
-				if (response) {
-					fetch('http://localhost:3000/image', {
-						method: 'put',
-						headers: { 'Content-Type': 'application/json' },
-						body: JSON.stringify({
-							id: this.state.user.id
-						})
-					})
-						.then((response) => response.json())
-						.then((count) => {
-							this.setState(Object.assign(this.state.user, { entries: count }));
-						});
-				}
-				this.displayFaceBox(this.calculateFaceLocation(response));
+		fetch('https://limitless-meadow-36904.herokuapp.com/imageurl', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				input: this.state.input
 			})
-			.catch((err) => console.log(err));
+		}).then((response) =>
+			response
+				.json()
+				.then((response) => {
+					if (response) {
+						fetch('https://limitless-meadow-36904.herokuapp.com:3000/image', {
+							method: 'put',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({
+								id: this.state.user.id
+							})
+						})
+							.then((response) => response.json())
+							.then((count) => {
+								this.setState(Object.assign(this.state.user, { entries: count }));
+							})
+							.catch(console.log);
+					}
+					this.displayFaceBox(this.calculateFaceLocation(response));
+				})
+				.catch((err) => console.log(err))
+		);
 	};
 
 	onRouteChange = (route) => {
 		if (route === 'signout') {
-			this.setState({ isSignedIn: false });
+			this.setState(initialState);
 		} else if (route === 'home') {
 			this.setState({ isSignedIn: true });
 		}
